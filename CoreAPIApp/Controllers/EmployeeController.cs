2c2734ceb3c4afccb2ced4b87ffad35fb1ad5323
Mukunnd Thakur <mukundv98@gmail.com>
2020-01-21 01:17:44 +0530

    adding files

---
+++ CoreAPIApp/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace CoreAPIApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+		[HttpGet]
+		public HttpResponseMessage EmpXML()
+		{
+			XmlDocument doc = new XmlDocument();
+			doc.Load(@"/Users/hp/Documents/TestAPI/CoreAPIApp/CoreAPIApp/App_Data/EmployeeTest.xml");
+
+			HttpResponseMessage response = new HttpResponseMessage();
+
+			string json = JsonConvert.SerializeXmlNode(doc);
+			response.Content = new StringContent(json, Encoding.UTF8, "application/json");
+
+			return response;
+		}
+	}
+}
---
+++ CoreAPIApp/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAPIApp.Models
+{
+	public class Employee
+	{
+		public string ID { get; set; }
+
+		public string Cmp_Name { get; set; }
+
+		public string Address { get; set; }
+	}
+}
